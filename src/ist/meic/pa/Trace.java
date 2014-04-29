8cd83a3711088e3d1c8b47cadb326d9853c43b12
Alberto Filipe Carvalho <lusoafc@gmail.com>
2014-04-29 16:47:54 +0100

    Project skeleton added

---
+++ src/ist/meic/pa/Trace.java
@@ -0,0 +1,12 @@
+package ist.meic.pa;
+
+public class Trace {
+
+	public Trace() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static public void print(Object object) {
+		// TODO
+	}
+}
---
+++ src/ist/meic/pa/TraceVM.java
@@ -0,0 +1,12 @@
+package ist.meic.pa;
+
+public class TraceVM {
+
+	public TraceVM() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
