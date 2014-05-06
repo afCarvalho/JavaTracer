76bbca7c043ef4bbdb963ef8167295cd4c81a244
ist164804 <jose.cavalheiro@tecnico.ulisboa.pt>
2014-05-06 11:23:46 +0000

    Batch file updated

--- tests.bat
+++ tests.bat
@@ -1,9 +1,11 @@
-cd classes
+javac -cp .;tracer.jar;lib/javassist.jar ./src/ist/meic/pa/traceinfo/*.java ./src/ist/meic/pa/*.java ./src/*.java -d ./classes
+cd classes 
 java -cp .;tracer.jar;../lib/javassist.jar ist.meic.pa.TraceVM Test0 > test0.out 2>&1
 java -cp .;tracer.jar;../lib/javassist.jar ist.meic.pa.TraceVM Test1 > test1.out 2>&1
 java -cp .;tracer.jar;../lib/javassist.jar ist.meic.pa.TraceVM Test2 > test2.out 2>&1
-cd ..
 
-FC /A classes\test0.out test0.expected
-FC /A classes\test1.out test1.expected
-FC /A classes\test2.out test2.expected
+FC /A test0.out ../test0.expected
+FC /A test1.out ../test1.expected
+FC /A test2.out ../test2.expected
+
+cd ..
