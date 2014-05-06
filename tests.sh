ec5fd3590aa8924c9e85109611f7515b971489cf
ist164804 <jose.cavalheiro@tecnico.ulisboa.pt>
2014-05-06 15:23:34 +0100

    Batch file updated

---
+++ tests.sh
@@ -0,0 +1,12 @@
+rm -rf classes;
+rm -rf bin;
+mkdir classes;
+javac -cp .:tracer.jar:lib/javassist.jar ./src/ist/meic/pa/traceinfo/*.java ./src/ist/meic/pa/*.java ./src/*.java -d ./classes;
+cd classes;
+java -cp .:tracer.jar:../lib/javassist.jar ist.meic.pa.TraceVM Test0 >test0.out;
+java -cp .:tracer.jar:../lib/javassist.jar ist.meic.pa.TraceVM Test1 >test1.out;
+java -cp .:tracer.jar:../lib/javassist.jar ist.meic.pa.TraceVM Test2 >test2.out;
+diff test0.out ../test0.expected;
+diff test1.out ../test1.expected;
+diff test2.out ../test2.expected;
+cd ..;
